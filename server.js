d572c10609d6f165ef8ab9b5096caa29ae8d8aea
salman <salmanumar04@icloud.com>
2026-03-06 17:43:40 +0500

    Added session and body-parser middleware

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+
 const connectDB = require("./config/db");
 
 const app = express();
 
 connectDB();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: "secretkey",
+    resave: false,
+    saveUninitialized: true
+}));
 
 app.get("/", (req, res) => {
     res.send("Login System Running");
