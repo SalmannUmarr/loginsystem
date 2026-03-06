c381b2c16500e7d0078310d8ffd407c5ced59905
salman <salmanumar04@icloud.com>
2026-03-06 17:45:35 +0500

    Added register route to save user in MongoDB

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 
 const connectDB = require("./config/db");
+const User = require("./classes/User");
 
 const app = express();
 
+// connect to MongoDB
 connectDB();
 
+// middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(session({
@@ -16,10 +19,26 @@ app.use(session({
     saveUninitialized: true
 }));
 
+// home route
 app.get("/", (req, res) => {
     res.send("Login System Running");
 });
 
+// register route
+app.post("/register", async (req, res) => {
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = new User(username, password);
+
+    const result = await user.register();
+
+    res.send(result);
+
+});
+
+// start server
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
