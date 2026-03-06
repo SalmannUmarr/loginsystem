239af3d7b1e8bb923b306af9521e206d7176d23e
salman <salmanumar04@icloud.com>
2026-03-06 17:38:54 +0500

    Added MongoDB connection using Mongoose

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+function connectDB() {
+
+    mongoose.connect("mongodb://127.0.0.1:27017/studentDB")
+        .then(() => {
+            console.log("MongoDB Connected");
+        })
+        .catch((err) => {
+            console.log("Database Error:", err);
+        });
+
+}
+
+module.exports = connectDB;
